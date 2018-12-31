0cfb3178ceaf5fc7f01ff8f945f10ca0e734a48e
Christian Loera <mark@ELCAP.attlocal.net>
2018-12-30 22:00:11 -0600

    Remove enum

--- city/city_curl.h
+++ city/city_curl.h
@@ -6,14 +6,6 @@
 
 #include "city.h"
 
-typedef enum get_city_by_s{
-    GET_BY_ID = 0,
-    GET_BY_ZIPCODE,
-    GET_BY_CITY,
-    GET_BY_LATLOT,
-    GET_FAILED
-} get_city_by;
-
 typedef struct city_info_s {
     size_t size;
     char *data;

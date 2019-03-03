ac8147964152443efcf4bfe4aa32ee9dc842eefd
Christian Loera <chatoloera18@gmail.com>
2019-03-03 15:56:24 -0600

    Init commit for API

---
+++ net/src/api/net_api.c
@@ -0,0 +1,7 @@
+#include "net_api.h"
+
+static bool interent_avail = false;
+
+const bool has_internet_access( void ) {
+   return internet_avail;
+}
---
+++ net/src/include/net_api.h
@@ -0,0 +1,8 @@
+#ifndef _NET_API_H_
+#define _NET_API_H
+
+#include <stdbool.h>
+
+const bool has_internet_access( void );
+
+#endif /* _NET_API_H_ */

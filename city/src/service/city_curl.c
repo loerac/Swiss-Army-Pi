75a5428298170fbd1cd4a7dafa7688b6dbeb8c40
Christian Loera <chatoloera18@gmail.com>
2019-03-24 11:35:26 -0500

    Updated URL configuration

--- city/src/service/city_curl.c
+++ city/src/service/city_curl.c
@@ -5,105 +5,91 @@
 
 #include "city_curl.h"
 #include "city_types.h"
+#include "city_custom.h"
 
-#define WEATHER_URL      "https://api.openweathermap.org/data/2.5/weather?"
-#define MAX_UNITS_STRING 7U
-#define MAX_APPID_STRING 7U
-#define MAX_GETBY_METHOD 64U
-
-static char *url_string = "";
-static size_t url_size = 0;
+static char url_str[512U] = {0};
 static city_info city = { '\0' };
 
-void url_write(const size_t size, const int string, ...) {
-    int i;
-    va_list al;
-    va_start(al, string);
-    url_size += size;
+static size_t write_callback(void *contents, size_t size, size_t nmemb, void *userp) {
+   size_t realsize = size * nmemb;
+   city_info *tmp_city= (city_info *)userp;
+   char *ptr = realloc(tmp_city->data, tmp_city->size + realsize + 1);
 
-    for (i = 1; i <= string; i++) {
-        if (strncmp(url_string, "", url_size) == 0) {
-            url_string = malloc(sizeof(char*) * url_size);
-        } else {
-            url_string = realloc(url_string, url_size);
-        }
-        (void)strncat(url_string, va_arg(al, char*), url_size);
-    }
+   if (NULL == ptr) {
+      printf("EMERG: Re-allocating memory failed - error(%m)\n");
+      realsize = 0;
+   } else {
+      tmp_city->data = ptr;
+      memcpy(&(tmp_city->data[tmp_city->size]), contents, realsize);
+      tmp_city->size += realsize;
+      tmp_city->data[tmp_city->size] = 0;
+   }
+
+   return realsize;
 }
 
-void destroyCity( void ) {
-    url_size = 0;
-    city.size = 0;
-    if ( url_string[0] != '\0' ) {
-        free(url_string);
-    }
-    if ( city.data[0] != '\0' ) {
-        free(city.data);
-    }
-}
+static bool url_configuration(const url_sts *url) {
+   bool ok = false;
+   city_list_s *format_list = get_format();
+   city_list_s *search_list = get_location();
 
-size_t writeMemoryCb(void *contents, size_t size, size_t nmemb, void *userd) {
-    size_t real_size = size * nmemb;
-    city_info *c = (city_info*)userd;
-    char *ptr = malloc(c->size + real_size + 1);
-    if (ptr == NULL) {
-        printf("Not enough memory (realloc returned NULL)\n");
-        goto exit;
-    }
+   if (NULL != search_list) {
+      const city_search_s *search = (city_search_s *)search_list->data;
+      char format_str[27] = {0};
+      while (NULL != format_list) {
+         const city_format_s *format = (city_format_s *)format_list->data;
+         (void)strncat(format_str, format->data, format->size);
+         format_list = city_list_next(format_list);
+      }
+      (void)snprintf(url_str, sizeof(url_str), "%s%s%s&appid=%s%c", url->url, search->data, format_str, url->key, '\0');
+      printf("url_str: %s\n", url_str);
+      ok = true;
+   }
 
-    c->data = ptr;
-    memcpy(&(c->data[c->size]), contents, real_size);
-    c->size += real_size;
-    c->data[c->size] = 0;
-
-exit:
-    return real_size;
-}
-
-bool findBy(char *gb, const url_sts *url) {
-   // TODO: Change find method
+   return ok;
 }
 
 bool weatherURL(const url_sts *url) {
-    CURLcode res;
-    CURL *curl_handle;
-    bool failed = true;
-    char url_search_city[MAX_GETBY_METHOD];
+   CURLcode res;
+   CURL *curl_handle;
+   bool ok = false;
 
-    int find_by = findBy(url_search_city, url);
-    if ( find_by == 0 ) {
-        printf("No valid key given to search city\n");
-        goto exit;
-    }
+   /*
+   * Configure the URL with the main URL,
+   * search location type, any format type,
+   * and the API key.
+   */
+   url_configuration(url);
 
-    url_write(strlen(WEATHER_URL) + 1, 1, WEATHER_URL);
-    url_write(strlen(url_search_city) + 1, 1, url_search_city);
-    url_write(MAX_UNITS_STRING + strlen(url->s_format) + 1, 2, "&units=", url->s_format);
-    url_write(MAX_APPID_STRING + strlen(url->key) + 1, 2, "&appid=", url->key);
+   curl_global_init(CURL_GLOBAL_ALL);
+   curl_handle = curl_easy_init();
 
-    curl_global_init(CURL_GLOBAL_ALL);
-    curl_handle = curl_easy_init();
+   curl_easy_setopt(curl_handle, CURLOPT_URL, url_str);
+   curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_callback);
+   curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&city);
+   curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+   res = curl_easy_perform(curl_handle);
 
-    curl_easy_setopt(curl_handle, CURLOPT_URL, url_string);
-    curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, writeMemoryCb);
-    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&city);
-    curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
-    res = curl_easy_perform(curl_handle);
+   if (res != CURLE_OK) {
+      printf("ERR: curl_easy_perform() failed - error(%s)\n", curl_easy_strerror(res));
+   } else {
+      curl_easy_cleanup(curl_handle);
+      ok = true;
+   }
 
-    if (res != CURLE_OK) {
-        fprintf(stderr, "curl_easy_perform() failed: error(%s)\n", curl_easy_strerror(res));
-        goto exit;
-    }
+   curl_global_cleanup();
 
-    curl_easy_cleanup(curl_handle);
-    curl_global_cleanup();
-    failed = false;
-
-exit:
-    return failed;
+   return ok;
 }
 
 
 const city_info getCityInfo( void ) {
-    return city;
+   return city;
+}
+
+void destroyCity( void ) {
+   city.size = 0;
+   if ( city.data[0] != '\0' ) {
+      free(city.data);
+   }
 }

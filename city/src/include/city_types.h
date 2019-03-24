9434b15c85a5e153676de145647e5494a66014fc
Christian Loera <chatoloera18@gmail.com>
2019-03-24 11:37:21 -0500

    Added enum value

--- city/src/include/city_types.h
+++ city/src/include/city_types.h
@@ -4,24 +4,25 @@
 #include <stdio.h>
 
 typedef enum city_init_s {
-    CITY_OK = 0,
-    CITY_FILE_PROCESS_NOK,
-    CITY_PARSE_CUSTOM_NOK,
-    CITY_WEATHER_URL_NOK
+   CITY_OK = 0,
+   CITY_FILE_PROCESS,
+   CITY_PARSE_CUSTOM,
+   CITY_WEATHER_URL,
+   CITY_WEATHER_PARSE
 } city_init;
 
 typedef enum city_search_e {
-    CITY_SEARCH_NONE = 0,
-    CITY_SEARCH_ID = 1,
-    CITY_SEARCH_NAME = 2,
-    CITY_SEARCH_ZIP = 4,
-    CITY_SEARCH_COORD = 8
+   CITY_SEARCH_NONE = 0,
+   CITY_SEARCH_ID = 1,
+   CITY_SEARCH_NAME = 2,
+   CITY_SEARCH_ZIP = 4,
+   CITY_SEARCH_COORD = 8
 } city_search_e;
 
 typedef enum city_format_e {
-    CITY_FORMAT_NONE = 0,
-    CITY_FORMAT_LANG = 1,
-    CITY_FORMAT_UNIT = 2
+   CITY_FORMAT_NONE = 0,
+   CITY_FORMAT_LANG = 1,
+   CITY_FORMAT_UNIT = 2
 } city_format_e;
 
 #endif

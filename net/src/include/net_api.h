6437334ab655715b60155ba8c368c6a31f4f1931
Christian Loera <chatoloera18@gmail.com>
2019-04-17 21:49:24 -0500

    Don't need the net_types.h anymore

--- net/src/include/net_api.h
+++ net/src/include/net_api.h
@@ -1,8 +1,26 @@
-#ifndef _NET_API_H_
-#define _NET_API_H
+#ifndef _NET_APIH_
+#define _NET_APIH_
 
 #include <stdbool.h>
 
-const bool net_internet_avail( void );
+#define MAX_DEV_LEN     32U
+#define MAX_SSID_LENGTH 32U
+
+typedef enum signal_strength {
+   SIGNAL_STRENGTH_STRONG,
+   SIGNAL_STRENGTH_GOOD,
+   SIGNAL_STRENGTH_WEAK,
+   SIGNAL_STRENGTH_BAD
+} signal_strength_e;
+
+typedef struct signal_info {
+   int level;
+   char ssid[MAX_SSID_LENGTH + 1U];
+   signal_strength_e strength;
+} signal_info_s;
+
+bool netInit( void );
+signal_info_s *getInternetInfo( void );
+const bool internetAvail( void );
 
 #endif /* _NET_API_H_ */

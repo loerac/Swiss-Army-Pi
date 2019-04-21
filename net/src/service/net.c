5aa7bce1d52a908609988546a8501df7bc33e96d
loerac <chatoloera18@gmail.com>
2019-04-21 12:07:05 -0500

    Not using struct pointer anymore, no need to

--- net/src/service/net.c
+++ net/src/service/net.c
@@ -18,22 +18,21 @@ int main(int argc, char *argv[]) {
       retval = 1;
       if (internetAvail()) {
          retval = 0;
-         signal_info_s *const sig_info = getInternetInfo();
-         //printf("Internet is available - wireless name(%s)\n", iwname);
-         printf("SSID------- %s\n", sig_info->ssid);
-         printf("Level------ %d dBm\n", sig_info->level);
+         signal_info_s sig_info = getInternetInfo();
+         printf("SSID------- %s\n", sig_info.ssid);
+         printf("Level------ %d dBm\n", sig_info.level);
 
-         if (sig_info->level <= -30) {
-            sig_info->strength = SIGNAL_STRENGTH_STRONG;
+         if (sig_info.level <= -30) {
+            sig_info.strength = SIGNAL_STRENGTH_STRONG;
             printf("Strong signal strength\n");
-         } else if ((-30 < sig_info->level) && (sig_info->level <= -60)) {
-            sig_info->strength = SIGNAL_STRENGTH_GOOD;
+         } else if ((-30 < sig_info.level) && (sig_info.level <= -60)) {
+            sig_info.strength = SIGNAL_STRENGTH_GOOD;
             printf("Good signal strength\n");
-         } else if ((-60 < sig_info->level) && (sig_info->level <= -70)) {
-            sig_info->strength = SIGNAL_STRENGTH_WEAK;
+         } else if ((-60 < sig_info.level) && (sig_info.level <= -70)) {
+            sig_info.strength = SIGNAL_STRENGTH_WEAK;
             printf("Reliable/weak signal strength\n");
          } else {
-            sig_info->strength = SIGNAL_STRENGTH_BAD;
+            sig_info.strength = SIGNAL_STRENGTH_BAD;
             printf("Unstable signal strength\n");
          }
       }

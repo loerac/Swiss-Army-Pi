fa066061886060e13e26d223506b27c081856b2d
Christian Loera <chatoloera18@gmail.com>
2019-07-15 14:03:27 -0500

    Updated function names

--- common_libraries/compat/src/time_compat.c
+++ common_libraries/compat/src/time_compat.c
@@ -5,7 +5,7 @@
 #include <string.h>
 
 /* See time_compat.h for description */
-ssize_t unixTimestampConvert( const time_t unix_timestamp,
+ssize_t timecompactUnixToDate( const time_t unix_timestamp,
                               const char *const format,
                               char *timestamp,
                               const size_t ts_length) {
@@ -24,14 +24,14 @@ ssize_t unixTimestampConvert( const time_t unix_timestamp,
 }
 
 /* See time_compat.h for description */
-char *getStringTimestamp(const char *const format) {
+char *timecompactStringTimestamp(const char *const format) {
    char *timestamp = NULL;
 
    if ('\0' == format[0]) {
       printf("NOTICE: Missing timestamp format\n");
    } else {
       char buff[1024] = {0};
-      ssize_t unix_ts = unixTimestampConvert(time(NULL), format, buff, sizeof(buff));
+      ssize_t unix_ts = timecompactUnixToDate(time(NULL), format, buff, sizeof(buff));
       timestamp = malloc(sizeof(char*) * unix_ts);
       if (NULL != timestamp) {
          istrncpy(timestamp, buff, unix_ts);
--- common_libraries/compat/src/time_compat.h
+++ common_libraries/compat/src/time_compat.h
@@ -6,9 +6,8 @@
 
 /**
  * INPUT:
- *    unix_timestamp - Unix timestamp
+ *    unix_ts - Unix timestamp
  *    format - Convert 'unix_timestamp' into specific format
- *    timestamp - Buffer for converted Unix timestamp
  *    ts_length - Buffer length of 'timestamp'
  * OUTPUT:
  *    timestamp - Holds the converted Unix timestamp on success,
@@ -18,7 +17,7 @@
  * DESCRIPTION:
  *    Convert an Unix timestamp to a specific format
  **/
-ssize_t unixTimestampConvert( const time_t unix_timestamp,
+ssize_t timecompactUnixToDate(const time_t unix_ts,
                               const char *const format,
                               char *timestamp,
                               const size_t ts_length);
@@ -33,6 +32,6 @@ ssize_t unixTimestampConvert( const time_t unix_timestamp,
  *
  *    ** USER IS RESPONSIBLE FOR FREEING MEMORY **
  **/
-char *getStringTimestamp(const char *const format);
+char *timecompactStringTimestamp(const char *const format);
 
 #endif /* _TIME_COMPAT_H_ */
